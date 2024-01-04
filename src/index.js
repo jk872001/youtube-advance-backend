ba4b2314ac8cb0569d454be3dfc5863b08ad7a2d
jk872001 <jiteshkumar872001@gmail.com>
2024-01-04 15:10:55 +0530

    utlis folder code add

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import express from "express";
 import dbConnect from "./db/index.js";
-import dotenv from "dotenv"
-dotenv.config()
+import { app } from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express();
-const PORT=process.env.PORT
+const PORT = process.env.PORT;
 
 dbConnect()
-
-
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
-
-app.listen(PORT,()=>{
-    console.log(`App is listening on port ${PORT}`)
-})
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDb connection error", err.message));
