a649634c797d9da88b8356a87a72359e648b670a
jk872001 <jiteshkumar872001@gmail.com>
2024-01-04 09:03:03 +0530

    mongoDb connection setup

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../../contants.js"
+
+
+
+const dbConnect=async()=>
+{
+    try{
+       const connectionInstance=await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+       console.log(`MongoDb connected successfully ! , DB HOST ${connectionInstance.connection.host}`)
+    }catch(err){
+          console.log("MongoDb connection error",err.message)
+          process.exit(1)
+    }
+}
+
+export default dbConnect;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import dbConnect from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config()
 
 const app=express();
 const PORT=process.env.PORT
 
+dbConnect()
 
 
 app.get("/",(req,res)=>{
